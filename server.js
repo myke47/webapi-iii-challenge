cb0cd524d6a37a5a33875462709027899fe975c5
myke47 <mychel.larson@gmail.com>
2019-07-30 21:44:47 -0700

    Added Posts DB

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
-const express = 'express';
+const express = require('express');
+const cors = require('cors');
+const posts = require('./data/helpers/postDb');
+const users = require('./data/helpers/userDb');
+const tags = require('./data/helpers/tagDb');
+const port = 5000;
 
 const server = express();
+server.use(express.json());
+server.use(cors({}));
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
+const errorHelper = (status, message, res) => {
+  res.status(status).json({ error: message });
+};
 
-//custom middleware
+// TODO Custom MiddleWare //
+
 
 function logger(req, res, next) {
 
 };
+
+// Endpoints
+server.get('/api/users', (req, res) => {
+  users
+  .get()
+  .then(foundUsers => {
+    res.json(foundUsers);
+  })
+  .catch(err => {
+    return errorHelper(500, 'Database not found', res);
+  });
+});
+
+
 
 module.exports = server;
